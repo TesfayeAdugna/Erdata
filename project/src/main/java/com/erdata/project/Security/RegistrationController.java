f789a0c99164a2f9b49850ae59bcb71fea94ad8b
Endalebob <endaleyohannes8@gmail.com>
2022-02-12 22:12:48 +0300

    hello

--- project/src/main/java/com/erdata/project/Security/RegistrationController.java
+++ project/src/main/java/com/erdata/project/Security/RegistrationController.java
@@ -2,11 +2,18 @@ package com.erdata.project.Security;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,12 +24,20 @@ public class RegistrationController {
     private final PasswordEncoder passwordEncoder;
 
     @GetMapping
-    public String registerForm() {
+    public String registerForm(Model model) {
+        model.addAttribute("form", new RegistrationForm());
         return "registration";
     }
 
     @PostMapping
-    public String processRegistration(RegistrationForm form) {
+    public String processRegistration(@Valid @ModelAttribute("form") RegistrationForm form, Errors errors, BindingResult bind) {
+
+        if (userRepository.findByUsername(form.getUsername()) != null){
+            bind.addError( new FieldError("form","username","User name already exist"));
+        }
+        if (errors.hasErrors()) {
+            return "registration";
+        }
         userRepository.save(form.toUser(passwordEncoder));
         return "redirect:/login";
     }
--- project/src/main/java/com/erdata/project/Security/RegistrationForm.java
+++ project/src/main/java/com/erdata/project/Security/RegistrationForm.java
@@ -1,19 +1,38 @@
 package com.erdata.project.Security;
 
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.Data;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class RegistrationForm{
+    @NotNull
+    @Size(min = 3, message = "Username must contain at least 3 characters")
     private String username;
+    @NotNull
+    @Size(min = 5, message = "Password must contain at least 5 characters")
     private String password;
+    @Length(min = 10,max = 10, message = "Phone number must contain 10 digits." )
+    @Digits(integer = 10, fraction = 0, message = "Phone number must be digits.")
     private String phone;
+    @NotNull
+    @Size(min = 3, message = "First name must contain at least 3 characters")
     private String firstName;
+
     private String middleName;
+    @NotNull
+    @Size(min = 3, message = "Last name must contain at least 3 characters")
     private String lastName;
+    @NotBlank(message = "Please choose gender")
     private String gender;
+    @NotBlank(message = "Address must not be blank.")
     private String address;
 
 
